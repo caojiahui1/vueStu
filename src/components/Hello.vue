9b347ae2d75e316bc0eb653d1cd516472ba9ad7f
caojiahui1 <1596663131@qq.com>
2017-06-05 18:54:04 +0800

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+
+    
+        <router-link to="/Hello2">跳到hello2</router-link>
+    
+      <mt-button type="default" v-text="btnMsg" @click="plus">default</mt-button>
+      <mt-button type="primary">primary</mt-button>
+      <mt-button type="danger">danger</mt-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      btnMsg:'确认'
+    }
+  },
+  methods : {
+    plus: function () {
+      alert(2)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+
+
+a {
+  color: #42b983;
+}
+</style>
