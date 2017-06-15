b575e502b3a57d84d88176ad62286a05ac210a0e
caojiahui1 <1596663131@qq.com>
2017-06-15 22:36:28 +0800

    afadad

--- src/page/demand/demand/demand.vue
+++ src/page/demand/demand/demand.vue
@@ -8,25 +8,27 @@
 <mu-row gutter style="margin-top: 10px;margin-bottom: 10px;">
     
 <mu-col width="5" tablet="50" desktop="33" >
+  
+  </mu-col>
+
+
+  <mu-col width="30" tablet="50" desktop="33" >
+    <select class="selSty" @change='selectClick()' v-model="demandTypeId">
+     <option :value="0">请选择</option>
+      <option v-for="item in typeList" :value="item.id">{{item.name}}</option>
+    </select>
+  </mu-col>
+{{demandTypeId}}
+<mu-col width="1" tablet="50" desktop="33" >
 
   </mu-col>
 
 
-  <mu-col width="40" tablet="50" desktop="33" >
-    <mu-select-field @change='selectClick()'  v-model="game1"  :labelFocusClass="['label-foucs']" :maxHeight="300"  fullWidth>
-    <mu-menu-item    v-for="text,index in typeList"   :id="text.id" :title='text.name' :value='text.name'  />
-  </mu-select-field>
-  </mu-col>
+  <mu-col width="30" tablet="50" desktop="33" >
+    <select class="selSty"  @change='selectClick()' v-model="supplyDemandType">
 
-<mu-col width="5" tablet="50" desktop="33" >
-
-  </mu-col>
-
-
-  <mu-col width="20" tablet="50" desktop="33" >
-    <mu-select-field v-model="game2" :labelFocusClass="['label-foucs']"  fullWidth>
-    <mu-menu-item v-for="text,index in list" :key="index" :id="text.id" :value="index.name" :title="text.name" />
-  </mu-select-field>
+      <option v-for="item in list" :value="item.id">{{item.name}}</option>
+    </select>
   </mu-col>
 
 <mu-col width="30" tablet="50" desktop="33" >
@@ -37,10 +39,10 @@
    
 
   <ul>
-    <li v-for="item in newsList"  style="display:flex;padding:10px 10px;">
-      <img :src="item.image_url" style="height:100%;width: 20%;"/>
+    <li v-for="item in demandList"  style="display:flex;padding:10px 10px;">
+      <!-- <img :src="item.image_url" style="height:100%;width: 20%;"/> -->
       <div style="display: flex; flex-direction:column;justify-content: flex-start;width:80%;">
-       <router-link :to='{path: "newsDetail", query: {id: item.id}}'  >{{item.title.substring(0,15)}}...</router-link>
+       <router-link :to='{path: "newsDetail", query: {id: item.demandId}}'  >{{item.title.substring(0,15)}}...</router-link>
       </div>
     </li>
 </ul>
@@ -59,20 +61,41 @@ export default {
   name: 'hello2',
   data () {
     return {
+      selected:0,
       newsList:[],
       pageNum:1,
       tempArr:[],
       btnClose:true,
-      game1: 0,
-      game2: 0,
-      list: [{name:'全部',id:0}, {name:'供',id:1}, {name:'求',id:2}],
-      typeList:[]
+      supplyDemandType:0,//供求类型 0 全部 1供 2求
+      demandTypeId:0,//需求类型id
+      list: [{name:'全部',id:0}, {name:'求',id:1}, {name:'供',id:2}],
+      typeList:[],//需求类型列表
+      demandList:[]  //资本需求列表
     }
   },
   
    methods : {
-    selectClick:function(){
-        
+    selectClick:function(event){
+      let that=this;
+               let listUrl='/api/remote/appDemand/findAPPDemandList?demandTypeId='+that.demandTypeId+'&supplyDemandType='+that.supplyDemandType+'&pageNum='+that.pageNum
+              this.$http.get(listUrl).then(function(data){
+           that.demandList=data.body.body.list
+          
+          // console.log(that.demandList[0].title)
+                },function(response){
+                    //console.info(response);
+                })
+    },
+        selectClick2:function(event){
+      let that=this;
+               let listUrl='/api/remote/appDemand/findAPPDemandList?demandTypeId='+that.demandTypeId+'&supplyDemandType='+that.supplyDemandType+'&pageNum='+that.pageNum
+              this.$http.get(listUrl).then(function(data){
+           that.demandList=data.body.body.list
+          
+          // console.log(that.demandList[0].title)
+                },function(response){
+                    //console.info(response);
+                })
     },
     plus: function () {
       router.go({name: '/', params: {userId: 1}});
@@ -83,12 +106,12 @@ export default {
        Indicator.open('加载中...');
        that.btnClose=false;
        //console.log(that.tempArr[0])
-       that.newsList =  that.newsList.concat(that.tempArr);
+       that.demandList =  that.demandList.concat(that.tempArr);
        that.tempArr=[];
 
       //start
       that.pageNum++;
-      var url='/api/remote/cms/findNewsPage?pageNum='+that.pageNum
+      var url='/api/remote/appDemand/findAPPDemandList?demandTypeId='+that.demandTypeId+'&supplyDemandType='+that.supplyDemandType+'&pageNum='+that.pageNum
       this.$http.get(url).then(function(data){
             that.tempArr=data.body.body.list;
             Indicator.close();
@@ -103,14 +126,14 @@ export default {
     store.commit('titlechange','资本需求') 
       Indicator.open('加载中...');
       let that=this;
-      let url='/api/remote/cms/findNewsPage?pageNum='+that.pageNum
+      let url='/api/remote/appDemand/findAPPDemandList?demandTypeId='+that.demandTypeId+'&supplyDemandType='+that.supplyDemandType+'&pageNum='+that.pageNum
       this.$http.get(url).then(function(data){
-           that.newsList=data.body.body.list;
+           that.demandList=data.body.body.list;
           // console.log(that.newsList)
            Indicator.close();
 //start
            that.pageNum++;
-      let url='/api/remote/cms/findNewsPage?pageNum='+that.pageNum
+      let url='/api/remote/appDemand/findAPPDemandList?demandTypeId='+that.demandTypeId+'&supplyDemandType='+that.supplyDemandType+'&pageNum='+that.pageNum
       this.$http.get(url).then(function(data){
            //console.log(data.body.body.list)
            that.tempArr=data.body.body.list;
@@ -123,33 +146,52 @@ export default {
           })
 
 
-//类型数据
+//类型数据 remote/appDemand/findAPPDemandList
        let typeUrl='/api/remote/appDemand/getDemandTypeList';
               this.$http.get(typeUrl).then(function(data){
-                
-          // console.log(data.body.body.queryDemandTypeList)
            let tempList=data.body.body.queryDemandTypeList
-          //that.typeList=that.typeList.concat(tempList);
           that.typeList=tempList
-          // console.log('哈哈哈')
-          // console.log(tempList)
-          // console.log(that.typeList)
-            // for(var i=0;i<tempList.length;i++){
-            //   that.typeList.push(tempList[i].name);
-            //   console.log(tempList[i].name)
-            // }
-           
                 },function(response){
                     //console.info(response);
                 })
-     
-       
-        console.log(that.game1)
+
+ //列表数据
+//  dateString
+// :
+// "2017-05-15"
+// demandId
+// :
+// 484
+// demandTypeId
+// :
+// 8
+// demandTypeString
+// :
+// "收购并购"
+// pv
+// :
+// 752
+// supplyDemandString
+// :
+// "求"
+// supplyDemandType
+// :
+// 1
+// title
+// :
+// "【1.6亿资金投资】有明确退出途径的文化科技类新三板公司"
+
+        let listUrl='/api/remote/appDemand/findAPPDemandList?demandTypeId='+that.demandTypeId+'&supplyDemandType='+that.supplyDemandType+'&pageNum='+that.pageNum
+              this.$http.get(listUrl).then(function(data){
+           that.demandList=data.body.body.list
+          
+          console.log(that.demandList[0].title)
+                },function(response){
+                    //console.info(response);
+                })
 
 
      }
-
-    
 
 }
 </script>
@@ -161,4 +203,8 @@ ul{padding: 0;}
 li{list-style: none;height: 80px;text-align: center;color: #666;border: 1px solid #e3e3e3;border-radius: 5px;background: #e3e3e3;margin-top: 10px;}
 .imgSize{width:100%;height:100%;}
 a{overflow: hidden;}
+.selSty{
+    width: 100%;
+    height: 30px;
+    line-height: 30px;}
 </style>
